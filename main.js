53da6aa426dc785446106c8d1ed3e2fb27f5576d
shrdst <148563908+shrdst@users.noreply.github.com>
2026-02-10 10:15:26 +0100

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,6 @@ const express = require("express");
 const app = express();
 
 app.use(express.static("public"));
-// require("dotenv").config();
 
 const serverPort = process.env.PORT || 3000;
 const server = http.createServer(app);
@@ -20,54 +19,36 @@ server.listen(serverPort);
 console.log(`Server started on port ${serverPort} in stage ${process.env.NODE_ENV}`);
 
 wss.on("connection", function (ws, req) {
-  console.log("Connection Opened");
-  console.log("Client size: ", wss.clients.size);
-
   if (wss.clients.size === 1) {
-    console.log("first connection. starting keepalive");
     keepServerAlive();
   }
 
   ws.on("message", (data) => {
     let stringifiedData = data.toString();
     if (stringifiedData === 'pong') {
-      console.log('keepAlive');
       return;
     }
     broadcast(ws, stringifiedData, false);
   });
 
-  ws.on("close", (data) => {
-    console.log("closing connection");
-
+  ws.on("close", () => {
     if (wss.clients.size === 0) {
-      console.log("last client disconnected, stopping keepAlive interval");
       clearInterval(keepAliveId);
     }
   });
 });
 
-// Implement broadcast function because of ws doesn't have it
 const broadcast = (ws, message, includeSelf) => {
-  if (includeSelf) {
-    wss.clients.forEach((client) => {
-      if (client.readyState === WebSocket.OPEN) {
+  wss.clients.forEach((client) => {
+    if (client.readyState === WebSocket.OPEN) {
+      if (includeSelf || client !== ws) {
         client.send(message);
       }
-    });
-  } else {
-    wss.clients.forEach((client) => {
-      if (client !== ws && client.readyState === WebSocket.OPEN) {
-        client.send(message);
-      }
-    });
-  }
+    }
+  });
 };
 
-/**
- * Sends a ping message to all connected clients every 50 seconds
- */
- const keepServerAlive = () => {
+const keepServerAlive = () => {
   keepAliveId = setInterval(() => {
     wss.clients.forEach((client) => {
       if (client.readyState === WebSocket.OPEN) {
@@ -76,8 +57,3 @@ const broadcast = (ws, message, includeSelf) => {
     });
   }, 50000);
 };
-
-
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
